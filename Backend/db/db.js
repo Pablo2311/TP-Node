117c2b694b3976c23d73c95049b447b113a04d2e
Pablo2311 <gutierrezpabloraul@gmail.com>
2024-07-12 16:13:26 -0300

    [FIX] db credentials

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "root",
+    user: "",
+    password: "",
     port: 3306,
     // database: "medical_db"
 });
